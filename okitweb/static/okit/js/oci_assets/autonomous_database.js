6ac0f0824e76f7689fc49713490040a44bb7a787
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2019-10-23 10:10:09 +0100

    Add admin password to autonomous Database

--- okitweb/static/okit/js/oci_assets/autonomous_database.js
+++ okitweb/static/okit/js/oci_assets/autonomous_database.js
@@ -46,7 +46,8 @@ function addAutonomousDatabase(parent_id, compartment_id) {
     autonomous_database['compartment_id'] = parent_id;
     autonomous_database['id'] = id;
     autonomous_database['display_name'] = generateDefaultName(autonomous_database_prefix, autonomous_database_count);
-    autonomous_database['db_name'] = autonomous_database['display_name']
+    autonomous_database['db_name'] = autonomous_database['display_name'];
+    autonomous_database['admin_password'] = generatePassword();
     autonomous_database['data_storage_size_in_tbs'] = 1;
     autonomous_database['cpu_core_count'] = 1;
     autonomous_database['db_workload'] = 'OLTP';
