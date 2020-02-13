e42fd72239354645bb8dda64827d502d426c19e7
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2020-02-13 11:35:14 +0000

    Convert multiple scripts to single flag driven start script that defaults to nginx

---
+++ docker/connect-bash-shell.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copyright © 2020, Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+export BASENAME=$(basename $0)
+export DIRNAME=$(dirname $0)
+export FILENAME="${BASENAME%.*}"
+
+source $(dirname $0)/docker-env.sh
+
+
+# Exec command
+docker exec \
+       -it \
+       start-okit-server \
+       /bin/bash
+
+docker ps -l
