e08aae6a1eb7eebb58b5abcd41c2ae3ba8dd3f73
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2020-04-22 10:54:04 +0100

    Clean Artifact Code

--- okitweb/static/okit/js/oci_assets/internet_gateway.js
+++ okitweb/static/okit/js/oci_assets/internet_gateway.js
@@ -81,7 +81,6 @@ class InternetGateway extends OkitArtifact {
         let position = 1;
         let definition = this.newSVGDefinition(this, this.getArtifactReference());
         let dimensions = this.getDimensions();
-        //let first_child = this.getParent().getTopEdgeChildOffset();
         let first_child = this.getParent().getChildOffset(this.getArtifactReference());
         definition['svg']['x'] = first_child.dx;
         definition['svg']['y'] = first_child.dy;
