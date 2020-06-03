c4e3b5a0fe332df16b8ab1cb7317854f37ed5617
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2020-06-03 16:14:43 +0100

    Add copyright to script

--- containers/scripts/build_docker_image_win.ps1
+++ containers/scripts/build_docker_image_win.ps1
@@ -1,4 +1,7 @@
 
+# Copyright (c) 2020, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 #Create Docker Image on Windows without sh shell installed
 
 $DOCKERIMAGE="okit"
--- containers/scripts/start_okit_server_win.ps1
+++ containers/scripts/start_okit_server_win.ps1
@@ -1,4 +1,7 @@
 
+# Copyright (c) 2020, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 #Start Docker image on windows
 #Assuming that OCI CLI it's configured in default path - oci setup config
 
