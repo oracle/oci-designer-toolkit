e88e63c52d3ade4e7881df22c0351d05d9ad2599
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2020-07-22 16:51:00 +0100

    Update ids within fragment json

--- okitweb/static/okit/model/js/artefacts/fragment.js
+++ okitweb/static/okit/model/js/artefacts/fragment.js
@@ -13,8 +13,10 @@ class Fragment extends  OkitJson {
      */
     constructor(target={}, okit_json={}) {
         super();
-        this.target = target;
-        this.okit_json = okit_json;
+        //this.target = target;
+        //this.okit_json = okit_json;
+        Object.defineProperty(this, 'target', { get: function() {return target;} });
+        Object.defineProperty(this, 'okit_json', { get: function() {return okit_json;} });
         console.info(this.target);
         console.info(this.okit_json);
     }
@@ -28,8 +30,53 @@ class Fragment extends  OkitJson {
     updateIds() {
         console.group('Updating Ids');
         // Regenerate all Ids so the fragment can be dropped multiple times
-        //this.updateCompartmentIds(fragment_json);
+        let id_map = {};
+        this.updatePrimaryIds(this, id_map);
+        this.updateIdAssociations(this, id_map);
         console.groupEnd();
+    }
+
+    updatePrimaryIds(fragment_json, id_map) {
+        let me = this;
+        if (Array.isArray(fragment_json) && fragment_json.length > 0 && typeof fragment_json[0] === 'object') {
+            fragment_json.forEach(function(e) {return me.updatePrimaryIds(e, id_map);});
+        } else if (typeof fragment_json === 'object' && fragment_json !== null) {
+            for (let [key, val] of Object.entries(fragment_json)) {
+                if (key === 'id') {
+                    id_map[val] = 'okit-fragment-' + uuidv4();
+                    fragment_json[key] = id_map[val];
+                } else if (!Array.isArray(val) && typeof val === 'object' && val !== null) {
+                    this.updatePrimaryIds(val, id_map);
+                } else if (Array.isArray(val)) {
+                    val.forEach(function(e) {return me.updatePrimaryIds(e, id_map);});
+                }
+            }
+        }
+    }
+
+    updateIdAssociations(fragment_json, id_map) {
+        let me = this;
+        if (Array.isArray(fragment_json) && fragment_json.length > 0 && typeof fragment_json[0] === 'object') {
+            fragment_json.forEach(function(e) {return me.updateIdAssociations(e, id_map);});
+        } else if (typeof fragment_json === 'object' && fragment_json !== null) {
+            for (let [key, val] of Object.entries(fragment_json)) {
+                if (key.endsWith('_id') || key.endsWith('_ids')) {
+                    if (Array.isArray(val)) {
+                        fragment_json[key] = val.map(function (e) {
+                            return id_map[e] ? id_map[e] : e;
+                        });
+                    } else if (typeof val === 'string') {
+                        fragment_json[key] = id_map[val] ? id_map[val] : fragment_json[key];
+                    }
+                } else if (!Array.isArray(val) && typeof val === 'object' && val !== null) {
+                    this.updateIdAssociations(val, id_map);
+                } else if (Array.isArray(val)) {
+                    val.forEach(function(e) {return me.updateIdAssociations(e, id_map);});
+                }
+            }
+        } else {
+            console.info(`Unknown Type : ${typeof fragment_json}`);
+        }
     }
 
 }
