73540da905c3e1853beffbe8f2e0de3abedb8cca
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2020-08-06 14:13:04 +0100

    OKE Skeleton

--- okitweb/static/okit/model/js/artefacts/oke.js
+++ okitweb/static/okit/model/js/artefacts/oke.js
@@ -14,7 +14,27 @@ class Oke extends OkitArtifact {
     constructor(data = {}, okitjson = {}) {
         super(okitjson);
         // Configure default values
-        this.name = this.generateDefaultName(okitjson.compartments.length + 1);
+        this.compartment_id = '';
+        this.display_name = this.generateDefaultName(okitjson.compartments.length + 1);
+        this.vcn_id = '';
+        this.kubernetes_version = '';
+        this.options = {
+            add_ons: {is_kubernetes_dashboard_enabled: false, is_tiller_enabled: false},
+            admission_controller_options: {is_pod_security_policy_enabled: false},
+            kubernetes_network_config: {pods_cidr: '', services_cidr: ''},
+            service_lb_subnet_ids: []
+        };
+        this.pools = [{
+            name: this.display_name + '_pool1',
+            node_config_details: {
+                placement_configs: [{availability_domain: 1, subnet_id: ''}],
+                size: 3
+            },
+            node_shape: 'VM.Standard.E2.1',
+            subnet_ids: [],
+            node_source_details: {os: 'Oracle Linux', version: '7.8', boot_volume_size_in_gbs: '50', source_type: 'image'},
+            ssh_public_key: ''
+        }];
         // Update with any passed data
         this.merge(data);
         this.convert();
