b8919d8c93f380cb2e3a1a7b58d31126cc06154a
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2020-10-22 11:35:07 +0100

    Add copyright

--- visualiser/model/okit_price_v2.py
+++ visualiser/model/okit_price_v2.py
@@ -1,3 +1,10 @@
+
+# Copyright (c) 2020, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+"""Provide Module Description
+"""
+
 import json
 import requests
 from model import shapes
