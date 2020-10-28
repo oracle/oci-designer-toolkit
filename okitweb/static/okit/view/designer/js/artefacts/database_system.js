f09fbf76c48ba3c89bef14dc60d0530970d2cc27
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2020-10-28 10:44:53 +0000

    Fix database minimum size for drawn artefacts

--- okitweb/static/okit/view/designer/js/artefacts/database_system.js
+++ okitweb/static/okit/view/designer/js/artefacts/database_system.js
@@ -14,8 +14,8 @@ class DatabaseSystemView extends OkitDesignerArtefactView {
 
     get parent_id() {return this.artefact.subnet_id;}
     get parent() {return this.getJsonView().getSubnet(this.parent_id);}
-    get minimum_width() {return 135;}
-    get minimum_height() {return 100;}
+    // TODO: Remove for new draw
+    get minimum_dimensions() {return {width: 135, height: 100};}
 
     /*
      ** SVG Processing
