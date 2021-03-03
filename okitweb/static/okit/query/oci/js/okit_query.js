e56ab4825ab086ab05eff841818c96a250fb3fae
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2021-03-03 15:19:05 +0000

    Fix undefined in Fast Discovery Title

--- okitweb/static/okit/query/oci/js/okit_query.js
+++ okitweb/static/okit/query/oci/js/okit_query.js
@@ -34,6 +34,7 @@ class OkitOCIQuery {
 
     queryAllResources(request) {
         console.info('------------- All Resources Query --------------------');
+        console.warn(request);
         let me = this;
         this.region_query_count[request.region] = 1;
         $.ajax({
@@ -45,7 +46,7 @@ class OkitOCIQuery {
             success: function(resp) {
                 console.log(resp)
                 const response_json = JSON.parse(resp);
-                const title = request.sub_compartments ? `Queried Compartment ${response_json.name} and Sub-Compartments` : `Queried Compartment ${response_json.name}`;
+                const title = request.sub_compartments ? `Queried Compartment ${request.compartment_name} and Sub-Compartments` : `Queried Compartment ${request.compartment_name}`;
                 const description = `${title} in Region ${request.region}`;
                 regionOkitJson[request.region].load(response_json)
                 regionOkitJson[request.region].title = title;
