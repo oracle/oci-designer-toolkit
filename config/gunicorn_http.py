76dac2077ad246988bfd33295c920d02578335d0
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2021-03-04 17:57:19 +0000

    Update gunicorn to use config file

---
+++ config/gunicorn_http.py
@@ -0,0 +1,4 @@
+bind = '0.0.0.0:80'
+workers = 4
+limit_request_line = 0
+timeout = 300
---
+++ config/gunicorn_https.py
@@ -0,0 +1,7 @@
+bind = '0.0.0.0:443'
+certfile = "/okit/ssl/okit.crt"
+keyfile = "/okit/ssl/okit.key"
+workers = 4
+limit_request_line = 0
+timeout = 300
+
