2408c9cc05a37ef29b669cfa0113ba88019ee2f5
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2021-04-28 17:41:17 +0100

    Add place holders for Terraform / Markdown / Ansible generators

---
+++ node/okit-node/src/generators/okit_terraform_generator.js
@@ -0,0 +1,18 @@
+/*
+** Copyright (c) 2020, 2021, Oracle and/or its affiliates.
+** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+*/
+
+/*
+** Author: Andrew Hopkinson
+*/
+
+import { OkitGenerator } from './okit_generator.js'
+import * as okit_resources from './terraform/resources.js'
+
+class OkitTerraformGenerator extends OkitGenerator {
+}
+
+export default OkitTerraformGenerator
+export { OkitTerraformGenerator }
+
