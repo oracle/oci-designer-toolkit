c533dc17cadb717a7b5b7bf23eb8ac3f7d334d0d
Stephen Cross <stephen.cross@oracle.com>
2021-08-03 19:35:38 -0400

    fix MySQL and DataFlow discovery for multi-region query

--- visualiser/discovery/oci_discovery_client.py
+++ visualiser/discovery/oci_discovery_client.py
@@ -1387,13 +1387,13 @@ class OciResourceDiscoveryClient(object):
                     # add
                     resources_by_region[region][resource_type] = final_resources_by_region[region][resource_type]
 
-        if len(resources_by_region) == 0:
-            logger.warn("Resource discovery results are empty")  
-        else:
-            # replace summary result with resource details
-            self.replace_resource_details(resources_by_region, region, "MySQLDbSystem", "MySQLDbSystemDetails")
-            self.replace_resource_details(resources_by_region, region, "DataFlowApplication", "DataFlowApplicationDetails")
-            self.replace_resource_details(resources_by_region, region, "DataFlowRun", "DataFlowRunDetails")
+            if len(resources_by_region) == 0:
+                logger.warn("Resource discovery results are empty")  
+            else:
+                # replace summary result with resource details
+                self.replace_resource_details(resources_by_region, region, "MySQLDbSystem", "MySQLDbSystemDetails")
+                self.replace_resource_details(resources_by_region, region, "DataFlowApplication", "DataFlowApplicationDetails")
+                self.replace_resource_details(resources_by_region, region, "DataFlowRun", "DataFlowRunDetails")
 
         # remove duplicate shapes
         # For multi-AD regions the list_shapes method returns shapes per AD, but does not distinguish which shape
