4768d9bf5819733784e3d58f227bb8b5d4874c68
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2022-03-09 10:49:38 +0000

    Initial V2 instance properties

--- okitweb/static/okit/js/okit.js
+++ okitweb/static/okit/js/okit.js
@@ -126,19 +126,6 @@ class OkitOCIData {
         this.compartments = [];
         const self = this;
         if (!this.loadLocal(profile, region)) this.query(profile, region)
-        // if (!this.loadLocal(profile, region)) {
-        //     const start = new Date().getTime()
-        //     $.getJSON(`dropdown/data/${String(profile)}/${String(region)}`, (resp) => {
-        //         const end = new Date().getTime()
-        //         console.info('Load Dropdown Data took', end - start, 'ms')
-        //         // $.extend(true, self, resp);
-        //         self.dropdown_data = resp;
-        //         self.storeLocal(profile, region);
-        //         if (resp.shipped && profile !== undefined) {
-        //             self.refresh(profile, region);
-        //         }
-        //     });
-        // }
     }
 
     refresh(profile, region='') {
@@ -294,6 +281,21 @@ class OkitOCIData {
             }
         }
         return [...new Set(oss)].sort();
+    }
+
+    getImageOSs() {
+        // let oss = []
+        // for (let image of this.dropdown_data.images) {
+        //     oss.push({compartment_id: image.compartment_id, id: image.operating_system, display_name: image.operating_system});
+        // }
+        const oss = this.dropdown_data.images.map(i => {return {compartment_id: i.compartment_id, id: i.operating_system, display_name: i.operating_system, platform: i.compartment_id === null}})
+        const unique = oss.reduce((unique, o) => {
+            if (!unique.some(obj => obj.platform === o.platform && obj.id === o.id)) {unique.push(o)}
+            return unique
+        }, [])
+        console.info(oss)
+        console.info(unique)
+        return unique.sort();
     }
 
     getInstanceOSVersions(os='') {
