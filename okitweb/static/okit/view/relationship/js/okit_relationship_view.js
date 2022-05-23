6f2e4184e31038c26c71e6f46774710ed85fafd7
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2022-05-23 15:26:27 +0100

    Clean code

--- okitweb/static/okit/view/relationship/js/okit_relationship_view.js
+++ okitweb/static/okit/view/relationship/js/okit_relationship_view.js
@@ -24,75 +24,19 @@ class OkitRelationshipJsonView extends OkitJsonView {
     get model() {return this.okitjson;}
     get data() {return this.oci_data;}
     get relationships() {
-        let nodes = []
-        let ids = []
-        let links = []
         if (this.model) {
+            // Identify Resources to display
             const resources = Object.entries(this.okitjson).filter(([k, v]) => !this.excluded.includes(k) && Array.isArray(v)).reduce((a, [k, v]) => [...a, ...v], [])
-            console.info('Resources', resources)
-            // Loop through Model elements and create node entries
-            // V2 
-            // resources.forEach((resource) => {
-            //     nodes.push({
-            //         id: this.getSafeId(resource.id),
-            //         ocid: resource.id,
-            //         name: resource.display_name,
-            //         type: resource.getArtifactReference()
-            //     });
-            //     ids.push(resource.id)
-            // })
-            nodes = resources.map((resource) => {return {id: this.getSafeId(resource.id), ocid: resource.id, name: resource.display_name, type: resource.getArtifactReference()}})
-            ids = resources.map((resource) => resource.id)
-            // V1
-            // Object.entries(this.okitjson).filter(([k, v]) => k !== 'compartments').forEach(([key, value]) => {
-            //     if (Array.isArray(value)) {
-            //         if (value.length > 0) {
-            //             for (let resource of value) {
-            //                 nodes.push({
-            //                     id: this.getSafeId(resource.id),
-            //                     ocid: resource.id,
-            //                     name: resource.display_name,
-            //                     type: resource.getArtifactReference()
-            //                 });
-            //                 ids.push(resource.id)
-            //             }
-            //         }
-            //     }
-            // })
-            // Assign id to index
-            // nodes.forEach((n, i) => n.id = i)
-            // Loop through Model elements and create link entries but only to ids in nodes
-            // V3
-            // resources.forEach((resource) => resource.getAssociations().forEach((id) => {if (ids.includes(id)) links.push({source: this.getSafeId(resource.id), target: this.getSafeId(id)})}))
-            links = resources.reduce((a, r) => [...a, ...r.getAssociations().filter((id) => ids.includes(id)).map((id) => {return {source: this.getSafeId(r.id), target: this.getSafeId(id)}})], [])
-            console.info('Links', links)
-            // V2
-            // Object.entries(this.okitjson).filter(([k, v]) => k !== 'compartments').forEach(([key, value]) => {
-            //     if (Array.isArray(value)) {
-            //         value.forEach((resource) => resource.getAssociations().forEach((id) => {if (ids.includes(id)) links.push({source: this.getSafeId(resource.id), target: this.getSafeId(id)})}))
-            //     }
-            // })
-            // V1 
-            // Object.entries(this.okitjson).filter(([k, v]) => k !== 'compartments').forEach(([key, value]) => {
-            //     if (Array.isArray(value)) {
-            //         if (value.length > 0) {
-            //             for (let resource of value) {
-            //                 Object.entries(resource).forEach(([k, v]) => {
-            //                     if (k.endsWith('_id') && ids.indexOf(v) >= 0) {
-            //                         links.push({source: this.getSafeId(resource.id), target: this.getSafeId(v)});
-            //                     } else if (k.endsWith('_ids')) {
-            //                         v.forEach((id) => {if (ids.indexOf(id) >= 0) links.push({source: this.getSafeId(resource.id),target: this.getSafeId(id)})});
-            //                     }
-            //                 });
-            //             }
-            //         }
-            //     }
-            // })
-            // Map Ids to index
-            // relationships.nodes = relationships.nodes.map((d) => {return {id: ids.indexOf(d.id), ocid: d.ocid, name: d.name, type: d.type}});
-            // relationships.nodes.forEach((n) => n.id = ids.indexOf(n.id));
+            // Create Nodes
+            const nodes = resources.map((resource) => {return {id: this.getSafeId(resource.id), ocid: resource.id, name: resource.display_name, type: resource.getArtifactReference()}})
+            // Get Ids of displayed resources
+            const ids = resources.map((resource) => resource.id)
+            // Create Links
+            const links = resources.reduce((a, r) => [...a, ...r.getAssociations().filter((id) => ids.includes(id)).map((id) => {return {source: this.getSafeId(r.id), target: this.getSafeId(id)}})], [])
+            return {nodes: nodes, links: links};
+        } else {
+            return {nodes: [], links: []};
         }
-        return {nodes: nodes, links: links};
     }
 
     static newView(model, oci_data=null, resource_icons=[], parent_id = 'relationship-div') {
@@ -143,12 +87,6 @@ class OkitRelationshipJsonView extends OkitJsonView {
     drawRelationshipSVG(width=800, height=800, for_export=false) {
         this.relationship_svg_div.selectAll('*').remove()
         const self = this;
-        // let parent_width  = $(jqId(this.parent_id)).width();
-        // let parent_height = $(jqId(this.parent_id)).height();
-        // if (!for_export) {
-        //     width = Math.round(Math.max(width, parent_width));
-        //     height = Math.round(Math.max(height, parent_height));
-        // }
         relationship_data = this.relationships;
         // Add SVG
         relationship_svg = this.relationship_svg_div.append("svg")
