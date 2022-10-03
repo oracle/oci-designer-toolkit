82734ef6141b14d5b95f959580ca0fc2be927213
Sofiane Mahdjoubi <sofiane.mahdjoubi@gmail.com>
2022-10-03 15:10:24 +0200

    updated way to get tenancy id

--- visualiser/okit_cloudshell.sh
+++ visualiser/okit_cloudshell.sh
@@ -6,5 +6,5 @@ export OKIT_DIR=~/okit
 file="$OKIT_DIR/log/okit.log"
 mkdir -p $(dirname $file) && touch "$file"
 pip install python-magic --user
-tenancy_id=$(curl -L -s 'http://169.254.169.254/opc/v1/instance/' | jq -r '.freeformTags."user-tenancy-ocid"')
+tenancy_id=$(oci iam compartment list --all --compartment-id-in-subtree true --access-level ACCESSIBLE --include-root --raw-output --query "data[?contains(\"id\",'tenancy')].id | [0]")
 python visualiser/okit_query.py -p $OCI_CLI_PROFILE -r $OCI_CLI_PROFILE -j okit.json -c $tenancy_id -t $tenancy_id -s
