ebd699749b203c4bbe393b379a16927dc1a579d5
Sofiane Mahdjoubi <sofiane.mahdjoubi@gmail.com>
2022-10-03 13:25:39 +0200

    adding magic module

--- visualiser/okit_cloudshell.sh
+++ visualiser/okit_cloudshell.sh
@@ -5,6 +5,6 @@ git checkout nightly
 export OKIT_DIR=~/okit
 file="$OKIT_DIR/log/okit.log"
 mkdir -p $(dirname $file) && touch "$file"
-#pip install python-magic --user
+pip install python-magic --user
 tenancy_id=$(curl -L -s 'http://169.254.169.254/opc/v1/instance/' | jq -r '.freeformTags."user-tenancy-ocid"')
 python okit_query.py -p eu-paris-1 -r eu-paris-1 -j okit.json -c $tenancy_id
