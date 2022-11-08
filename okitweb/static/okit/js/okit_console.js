7902144439f9618776fcbcfa904a046124c3d296
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2022-11-08 11:35:59 +0000

    Update Version Number

--- okitweb/static/okit/js/okit_console.js
+++ okitweb/static/okit/js/okit_console.js
@@ -5,7 +5,7 @@
 console.debug('Loaded Console Javascript');
 
 
-const okitVersion = '0.43.0 - nightly -';
+const okitVersion = '0.43.0';
 const okitReleaseDate = '9th November 2022';
 
 // Validation
