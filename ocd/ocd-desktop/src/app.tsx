9830bc8e84c4fbb89f6bcec7b4fc90c5948864ca
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2023-08-11 11:08:19 +0100

    Update desktop to be react app based

--- ocd/ocd-desktop/src/app.tsx
+++ ocd/ocd-desktop/src/app.tsx
@@ -1,7 +1,26 @@
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-render();
+export default App;
