431bcc5756435f21328fa389d058bca4cdb62c19
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2023-10-12 15:15:19 +0100

    Add Config state saving functionality

---
+++ ocd/packages/desktop/src/facade/OcdConfigFacade.ts
@@ -0,0 +1,19 @@
+/*
+** Copyright (c) 2020, 2023, Oracle and/or its affiliates.
+** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+*/
+
+import { OcdConsoleConfiguration } from "../components/OcdConsoleConfiguration"
+
+/*
+** Facade exists so we can switch between Electron based and Web based which will require a web server
+*/
+
+export namespace OcdConfigFacade {
+    export const loadConsoleConfig = (): Promise<any> => {
+        return window.ocdAPI ? window.ocdAPI.loadConsoleConfig() : new Promise((resolve, reject) => {reject('Currently Not Implemented')})
+    }
+    export const saveConsoleConfig = (config: OcdConsoleConfiguration): Promise<any> => {
+        return window.ocdAPI ? window.ocdAPI.saveConsoleConfig(config) : new Promise((resolve, reject) => {reject('Currently Not Implemented')})
+    }
+}
