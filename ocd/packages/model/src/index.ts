a42f0c0df1c5b4fb9744fe554739f9f827bc3aa7
Andrew Hopkinson <andrew.hopkinson@oracle.com>
2024-02-13 17:34:15 +0000

    Add Variable page

--- ocd/packages/model/src/index.ts
+++ ocd/packages/model/src/index.ts
@@ -4,7 +4,7 @@
 */
 
 export { OcdDesign, OcdViewPage, OcdViewCoords, OcdViewLayer, OcdBaseModel, OcdViewPoint, OcdViewConnector, OcdViewCoordsStyle } from './OcdDesign'
-export { OcdResources, OciResources } from './OcdDesign'
+export { OcdResources, OciResources, OcdVariable } from './OcdDesign'
 export { PaletteGroup, PaletteResource } from './OcdPalette'
 export { OcdResource } from './OcdResource'
 export { OciResource } from './provider/oci/OciResource'
