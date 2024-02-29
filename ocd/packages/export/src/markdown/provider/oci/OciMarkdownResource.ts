4d889dff2dfc30c77521c3ebba719416cb49a8c4
Andrew Hopkinson <64254061+toxophilist@users.noreply.github.com>
2024-02-29 15:58:49 +0000

    First cut of Markdown generated code

---
+++ ocd/packages/export/src/markdown/provider/oci/OciMarkdownResource.ts
@@ -0,0 +1,12 @@
+/*
+** Copyright (c) 2020, 2023, Oracle and/or its affiliates.
+** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+*/
+
+import OcdMarkdownResource from '../../OcdMarkdownResource'
+import { OciModelResources as Model, OciResource } from '@ocd/model'
+
+export class OciMarkdownResource extends OcdMarkdownResource {
+}
+
+export default OciMarkdownResource
---
+++ ocd/packages/export/src/markdown/provider/oci/resources.ts
@@ -0,0 +1,59 @@
+/*
+** Copyright (c) 2020, 2023, Oracle and/or its affiliates.
+** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+*/
+/*
+**
+** ======================================================================
+** === Auto Generated Code All Edits Will Be Lost During Regeneration ===
+** ======================================================================
+**
+*/
+
+export { OciAnalyticsInstance } from './resources/OciAnalyticsInstance'
+export { OciAutonomousDatabase } from './resources/OciAutonomousDatabase'
+export { OciBastion } from './resources/OciBastion'
+export { OciBucket } from './resources/OciBucket'
+export { OciCompartment } from './resources/OciCompartment'
+export { OciCpe } from './resources/OciCpe'
+export { OciDbSystem } from './resources/OciDbSystem'
+export { OciDhcpOptions } from './resources/OciDhcpOptions'
+export { OciDrg } from './resources/OciDrg'
+export { OciDrgAttachment } from './resources/OciDrgAttachment'
+export { OciDrgRouteDistribution } from './resources/OciDrgRouteDistribution'
+export { OciDrgRouteDistributionStatement } from './resources/OciDrgRouteDistributionStatement'
+export { OciDrgRouteTable } from './resources/OciDrgRouteTable'
+export { OciDrgRouteTableRouteRule } from './resources/OciDrgRouteTableRouteRule'
+export { OciFileSystem } from './resources/OciFileSystem'
+export { OciInstance } from './resources/OciInstance'
+export { OciInternetGateway } from './resources/OciInternetGateway'
+export { OciIpsec } from './resources/OciIpsec'
+export { OciKey } from './resources/OciKey'
+export { OciLoadBalancer } from './resources/OciLoadBalancer'
+export { OciLoadBalancerBackend } from './resources/OciLoadBalancerBackend'
+export { OciLoadBalancerBackendSet } from './resources/OciLoadBalancerBackendSet'
+export { OciLoadBalancerListener } from './resources/OciLoadBalancerListener'
+export { OciLocalPeeringGateway } from './resources/OciLocalPeeringGateway'
+export { OciMountTarget } from './resources/OciMountTarget'
+export { OciMysqlDbSystem } from './resources/OciMysqlDbSystem'
+export { OciNatGateway } from './resources/OciNatGateway'
+export { OciNetworkLoadBalancer } from './resources/OciNetworkLoadBalancer'
+export { OciNetworkSecurityGroup } from './resources/OciNetworkSecurityGroup'
+export { OciNetworkSecurityGroupSecurityRule } from './resources/OciNetworkSecurityGroupSecurityRule'
+export { OciNosqlIndex } from './resources/OciNosqlIndex'
+export { OciNosqlTable } from './resources/OciNosqlTable'
+export { OciOkeCluster } from './resources/OciOkeCluster'
+export { OciOkeNodePool } from './resources/OciOkeNodePool'
+export { OciPolicy } from './resources/OciPolicy'
+export { OciRemotePeeringConnection } from './resources/OciRemotePeeringConnection'
+export { OciRouteTable } from './resources/OciRouteTable'
+export { OciSecret } from './resources/OciSecret'
+export { OciSecurityList } from './resources/OciSecurityList'
+export { OciServiceGateway } from './resources/OciServiceGateway'
+export { OciSubnet } from './resources/OciSubnet'
+export { OciVault } from './resources/OciVault'
+export { OciVcn } from './resources/OciVcn'
+export { OciVnicAttachment } from './resources/OciVnicAttachment'
+export { OciVolume } from './resources/OciVolume'
+export { OciVolumeAttachment } from './resources/OciVolumeAttachment'
+    
