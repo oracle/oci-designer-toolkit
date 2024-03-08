53646a1b9621d5458775f6dbcbe747c0c27d35b7
Andrew Hopkinson <64254061+toxophilist@users.noreply.github.com>
2024-03-08 15:57:46 +0000

    Move AutoLayout to model package

--- ocd/packages/desktop/src/components/OcdDocument.ts
+++ ocd/packages/desktop/src/components/OcdDocument.ts
@@ -6,7 +6,7 @@
 import { v4 as uuidv4 } from 'uuid'
 import { OciModelResources } from '@ocd/model'
 import { OcdDesign, OcdViewPage, OcdViewCoords, OcdViewLayer, OcdBaseModel, OcdViewPoint, OcdViewCoordsStyle, OcdResource, OciResource, PaletteResource } from '@ocd/model'
-import { OcdAutoLayout } from './OcdAutoLayout'
+import { OcdAutoLayout } from '@ocd/model'
 import { OcdUtils } from '@ocd/core'
 import additionTitleInfo from '../data/OcdAdditionTitleInfo'
 
