b6b62ec67efed19d816e4c8485634240a07aaae1
Andrew Hopkinson <64254061+toxophilist@users.noreply.github.com>
2024-06-27 13:30:16 +0100

    chore(OKIT-267): update build date details

--- ocd/packages/export/src/data/OcdBuildDetails.ts
+++ ocd/packages/export/src/data/OcdBuildDetails.ts
@@ -12,10 +12,10 @@
 
 export const buildDetails: Record<string, string> = {
     version: '0.2.0',
-    date: '6/17/2024',
-    time: '3:07:44 PM',
-    datetime: '6/17/2024, 3:07:44 PM',
-    utc: 'Mon, 17 Jun 2024 14:07:44 GMT'
+    date: '6/27/2024',
+    time: '1:28:53 PM',
+    datetime: '6/27/2024, 1:28:53 PM',
+    utc: 'Thu, 27 Jun 2024 12:28:53 GMT'
 }
 
 export default buildDetails
