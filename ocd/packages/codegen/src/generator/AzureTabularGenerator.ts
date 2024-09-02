a227a895db13712891fa7a477228d7103a8ac0f6
Andrew Hopkinson <64254061+toxophilist@users.noreply.github.com>
2024-09-02 09:36:38 +0100

    feat(Azure): Add Azure Generators first cut

---
+++ ocd/packages/codegen/src/generator/AzureTabularGenerator.ts
@@ -0,0 +1,17 @@
+/*
+** Copyright (c) 2020, 2024, Oracle and/or its affiliates.
+** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+*/
+
+import { OcdTabularGenerator } from './OcdTabularGenerator.js'
+import { commonElements, commonIgnoreElements } from './data/AzureCommonResourceProperties.js'
+
+export class AzureTabularGenerator extends OcdTabularGenerator {
+    constructor () {
+        super('Azure')
+        this.ignoreAttributes = [...commonElements, ...commonIgnoreElements]
+    }
+}
+
+export default AzureTabularGenerator
+module.exports = { AzureTabularGenerator }
