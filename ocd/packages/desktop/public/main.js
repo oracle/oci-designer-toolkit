4e86e03b700c3edd7c12ba20592fcda088fb9353
Andrew Hopkinson <64254061+toxophilist@users.noreply.github.com>
2024-09-30 16:43:56 +0100

    refactor: user master library

--- ocd/packages/desktop/public/main.js
+++ ocd/packages/desktop/public/main.js
@@ -538,8 +538,8 @@ async function handleOpenExternalUrl(event, href) {
 }
 
 // Library / Reference Architecture Functions
-// const libraryUrl = 'https://raw.githubusercontent.com/oracle/oci-designer-toolkit/refs/heads/master/ocd/library'
-const libraryUrl = 'https://raw.githubusercontent.com/oracle/oci-designer-toolkit/refs/heads/toxophilist/sprint-dev/ocd/library'
+const libraryUrl = 'https://raw.githubusercontent.com/oracle/oci-designer-toolkit/refs/heads/master/ocd/library'
+// const libraryUrl = 'https://raw.githubusercontent.com/oracle/oci-designer-toolkit/refs/heads/toxophilist/sprint-dev/ocd/library'
 const libraryFile = 'referenceArchitectures.json'
 
 async function handleLoadLibraryIndex(event) {
